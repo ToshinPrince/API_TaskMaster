f85999f70191711aa7a653487d9e8c7efbea38a1
Toshin Prince <toshinprince@gmail.com>
2024-02-01 13:41:23 +0530

    setting up app.js test

--- app.js
+++ app.js
@@ -3,4 +3,8 @@ const app = express();
 
 const port = "3000";
 
+app.get("/hello", (req, res) => {
+  res.send("TaskMaster App");
+});
+
 app.listen(port, console.log(`App is listeniong on ${port}`));
