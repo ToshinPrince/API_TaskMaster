a208b62fd67624a37803de754fd51674c95cdc8c
Toshin Prince <toshinprince@gmail.com>
2024-02-17 12:57:10 +0530

    controllers:upadteTask

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -50,11 +50,23 @@ const deleteTask = async (req, res) => {
   }
 };
 
-const updateTask = (req, res) => {
-  res.send("Update Task");
+const updateTask = async (req, res) => {
+  // res.send("Update Task");
   try {
     const { id: taskId } = req.params;
-  } catch (error) {}
+    const task = await Task.findOneAndUpdate({ _id: taskId }, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!task) {
+      return res.status(404).json({ msg: `No task with id:${taskId}` });
+    }
+
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
 module.exports = { getAllTasks, createTask, getTask, deleteTask, updateTask };
